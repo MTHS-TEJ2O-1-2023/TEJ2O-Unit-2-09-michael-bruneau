0eadb486ab4fc9556685efd456a5368bef8a7bee
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-10 13:15:12 -0400

    broken

--- main.ts
+++ main.ts
@@ -11,3 +11,17 @@ basic.showIcon(IconNames.Happy)
 
 // variables
 let randomNumber: number = -1
+
+input.onGesture(Gesture.Shake, function() {
+  randomNumber = randint(0, 2)
+  basic.clearScreen()
+
+  // if randomNumber was 0
+  if (randomNumber == 0) {
+  basic.showIcon(IconNames.Scissors)
+}
+
+  // if randomNumber was 1
+  if (randomNumber == 1)
+  basic.showIcon(IconNames.Rock)
+})
