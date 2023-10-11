979105f473128de154243ac3a14de17c148f3eed
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-11 22:28:10 +0000

    Pythyon

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -4,6 +4,7 @@ Created on: Oct 2023
 This module is a Micro:bit MicroPython program
 """
 
+import random
 from microbit import *
 
 display.clear()
@@ -11,5 +12,20 @@ display.show(Image.HAPPY)
 
 while True:
     gesture = accelerometer.current_gesture()
-    if gesture == "shaken":
-        display.show(Image.YES)
+    if gesture == "face up":
+        random.randint(0, 2)
+        
+        if (random == 0):
+            display.show(Image.SCISSOR)
+            display.pause(1000)
+            display.show(Image.HAPPY)
+        
+        if (random == 1):
+            display.show(Image.SQUARE_SMALL)
+            display.pause(1000)
+            display.show(Image.HAPPY)
+        
+        if (random == 2):
+            display.show(Image.SQUARE)
+            display.pause(1000)
+            display.show(Image.HAPPY)
