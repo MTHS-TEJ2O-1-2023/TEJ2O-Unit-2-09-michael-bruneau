032a32b2c8beba92e2b2fafb290d6cb8a1a3f434
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-12 21:22:48 +0000

    python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -17,26 +17,22 @@ while True:
         display.show(Image.YES)
         if button_b.is_pressed():
             display.scroll(str(score))
-    
+
     gesture = accelerometer.current_gesture()
     if accelerometer.was_gesture("shake"):
         random_number = random.randint(0, 2)
-        
-        if (random_number == 0):
-            image = Image("99009:"
-                            "99090:"
-                            "00900:"
-                            "99090:"
-                            "99009:")
+
+        if random_number == 0:
+            image = Image("99009:" "99090:" "00900:" "99090:" "99009:")
             sleep(5000)
             display.show(Image.HAPPY)
 
-        if (random_number == 1):
+        if random_number == 1:
             display.show(Image.SQUARE_SMALL)
             sleep(5000)
             display.show(Image.HAPPY)
 
-        if (random_number == 2):
+        if random_number == 2:
             display.show(Image.SQUARE)
             sleep(5000)
             display.show(Image.HAPPY)
